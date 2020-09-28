3b27e11be596400b75e68d3c2e9917e61fedcf61
pikachu1106 <chiaochiao1106@gmail.com>
2020-09-29 05:19:01 +0800

    Updates

---
+++ precache-manifest.10201749a18c31923b25ef1f9a1252a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113db23134b5c88250ffae80190adbae",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7c5eeaf09301def87cfc",
+    "url": "/my-app/static/js/2.97b913e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.97b913e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af73542e1074c8395f52",
+    "url": "/my-app/static/js/main.fac20791.chunk.js"
+  },
+  {
+    "revision": "bcf1938cc6695259fe84",
+    "url": "/my-app/static/js/runtime-main.8cd56e33.js"
+  },
+  {
+    "revision": "446db347d3a9bd7fab84828b0051add3",
+    "url": "/my-app/static/media/main.446db347.jpg"
+  }
+]);
